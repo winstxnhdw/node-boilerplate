c40dfc22a4e60fbf1c3525ea5c51ca6cfc302576
winstxnhdw <winstxnhdw@gmail.com>
2023-05-09 21:47:01 +0800

    fix: fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,6 @@ import type {
 export const handler: Handler = async (
   event: APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>
 ): Promise<APIGatewayProxyResultV2> => {
+  await new Promise(() => ({}))
   return { statusCode: 200, body: event.body }
 }
